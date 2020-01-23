20aad77a85787ad9869950df6986c168d10cca16
Raphael Miranda <raphael.miranda@omio.com>
2020-01-23 23:38:54 +0100

    adding schema

--- src/test/kotlin/com/example/demo/DataInitializer.kt
+++ src/test/kotlin/com/example/demo/DataInitializer.kt
@@ -13,39 +13,14 @@ import org.springframework.stereotype.Component
 @Component
 class DataInitializer(private val databaseClient: DatabaseClient, private val postRepository: PostRepository) {
 
-
     @EventListener(value = [ApplicationReadyEvent::class])
     fun init() {
-        println("start data initialization  ...")
-        this.databaseClient.execute {
-            """ CREATE TABLE posts (
-                    id SERIAL PRIMARY KEY,
-                    title VARCHAR(255) NOT NULL,
-                    content VARCHAR(255) NOT NULL
-                );
-            """ }.then().and({
-            this.databaseClient.insert()
-                    .into("posts")
-                    //.nullValue("id", Long::class.java)
-                    .value("title", "First post title")
-                    .value("content", "Content of my first post")
-                    .then()
-                    .log()
-                    .thenMany(
-                            this.databaseClient.select()
-                                    .from("posts")
-                                    .orderBy(Sort.Order.by("id").with(Sort.Direction.DESC))
-                                    .`as`(Post::class.java)
-                                    .fetch()
-                                    .all()
-                                    .log()
-                    )
-        }).subscribe(null, null, { println("initialization is done...") })
         runBlocking {
-            postRepository.deleteAll()
-            postRepository.init()
+            val deletedCount = postRepository.deleteAll()
+            println(" $deletedCount posts deleted!")
+            postRepository.save(Post(title = "My first post title", content = "Content of my first post"))
+            postRepository.save(Post(title = "My second post title", content = "Content of my second post"))
         }
-
     }
 
 }
